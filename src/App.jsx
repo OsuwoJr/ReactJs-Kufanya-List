f0369ebd7d748b442d42c4a2b3682eab244009f8
OsuwoJr <johnfredosuwo@gmail.com>
2024-07-02 12:54:58 +0300

    activated the delete button
    handleDeleteTodo function

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,21 @@ function handleAddTodos(newTodo){
   const newTodoList = [...todos, newTodo]
   setTodos(newTodoList)
 }
+
+function handleDeleteTodo(index){
+  const newTodoList = todos.filter((todo, todoIndex) =>{
+    return todoIndex !== index
+  })
+  setTodos(newTodoList)
+}
+
+function handleEditTodo(index){
+
+}
   return (
     <>
     <TodoInput handleAddTodos={handleAddTodos}/>
-    <TodoList todos={todos}/>
-      
+    <TodoList handleDeleteTodo={handleDeleteTodo} todos={todos}/> 
     </>
   )
 }

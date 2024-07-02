a8ce7821ff1cd3b23fc72b9e659eff9434701f4f
OsuwoJr <johnfredosuwo@gmail.com>
2024-07-02 11:46:17 +0300

    Updated App.jsx
    created useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
 
 const App = () => {
-  let todos = [
+ 
+const [todos, setTodos] = useState ([
     'Skip Breakfast',
     'Skip lunch',
     'Eat Supper'
-]
+])
   return (
     <>
     <TodoInput/>
